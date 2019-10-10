564700951e1a1b419deaff7b3fa899dee1a4683e
ParkChunSo <cnsth123@gmail.com>
2019-10-10 21:36:47 +0900

    signup ui 구현중

--- app/src/main/java/com/example/gamsung/LoginActivity.java
+++ app/src/main/java/com/example/gamsung/LoginActivity.java
@@ -1,14 +1,37 @@
 package com.example.gamsung;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnStart, btnSignUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainHomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
